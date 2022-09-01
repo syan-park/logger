67338adaa2f213fdb65215a96f3e44910bacb010
swpark <64414447+syan-park@users.noreply.github.com>
2022-09-01 11:15:18 +0900

    Temporary edit : ansicolor is not working in iOS

--- lib/src/outputs/console_output.dart
+++ lib/src/outputs/console_output.dart
@@ -1,12 +1,33 @@
-import 'package:logger/src/logger.dart';
-import 'package:logger/src/log_output.dart';
+// import 'package:logger/src/logger.dart';
+// import 'package:logger/src/log_output.dart';
+
+// /// Default implementation of [LogOutput].
+// ///
+// /// It sends everything to the system console.
+// class ConsoleOutput extends LogOutput {
+//   @override
+//   void output(OutputEvent event) {
+//     event.lines.forEach(print);
+//   }
+// }
+
+import 'dart:io';
+import 'dart:developer' as developer;
+import 'package:flutter/foundation.dart';
+import 'package:logger/log_output.dart';
+import 'package:logger/logger.dart';
+
+const bool _kReleaseMode = kReleaseMode;
 
 /// Default implementation of [LogOutput].
-///
-/// It sends everything to the system console.
+/// /// /// It sends everything to the system console.
 class ConsoleOutput extends LogOutput {
   @override
   void output(OutputEvent event) {
-    event.lines.forEach(print);
+    if (_kReleaseMode || !Platform.isIOS) {
+      event.lines.forEach(debugPrint);
+    } else {
+      event.lines.forEach(developer.log);
+    }
   }
 }
